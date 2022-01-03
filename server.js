4c872ea7d5647a6c0965562c1152cbfca5fa523f
k-srihari <sriharik998@gmail.com>
2022-01-03 10:32:06 +0530

    server.js file beautified
    re-ordered get/post login/register
    code beautified with prettier

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 if (process.env.NODE_ENV !== 'production') {
-    dotenv.config()
+  dotenv.config()
 }
 import express from 'express'
 import bcrypt from 'bcrypt'
@@ -13,13 +13,15 @@ import override from 'method-override'
 const app = express()
 
 app.set('view-engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(flash())
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_KEY,
     resave: false,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+)
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(override('override_to'))
@@ -27,63 +29,68 @@ app.use(override('override_to'))
 const allUsers = []
 
 const checkAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next()
-    }
-    return res.redirect('/login')
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  return res.redirect('/login')
 }
 
 const checkNotAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return res.redirect('/')
-    }
-    return next()
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  return next()
 }
 
 app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.userName})
+  res.render('index.ejs', { name: req.user.userName })
 })
 
 app.get('/register', checkNotAuthenticated, (_req, res) => {
-    console.log("User Count: " + allUsers.length)
-    res.render('register.ejs')
-})
-
-app.post('/register', async (req, res) => {
-    try {
-        let hashedPass = await bcrypt.hash(req.body.password, 2)
-        let newUser = {
-            userID: Date.now().toString(),
-            userName: req.body.name,
-            emailID: req.body.email,
-            password: hashedPass
-        }
-        console.log(newUser)
-        allUsers.push(newUser)
-        res.redirect('/login')
-    } catch (error) {
-        res.sendStatus(500)
-    }
+  console.log('User Count: ' + allUsers.length)
+  res.render('register.ejs')
 })
 
 app.get('/login', checkNotAuthenticated, (_req, res) => {
-    console.log("User Count: " + allUsers.length)
-    res.render('login.ejs')
+  console.log('User Count: ' + allUsers.length)
+  res.render('login.ejs')
 })
 
-app.post('/login', passport.authenticate('local', {
+app.post('/register', async (req, res) => {
+  try {
+    let hashedPass = await bcrypt.hash(req.body.password, 2)
+    let newUser = {
+      userID: Date.now().toString(),
+      userName: req.body.name,
+      emailID: req.body.email,
+      password: hashedPass,
+    }
+    console.log(newUser)
+    allUsers.push(newUser)
+    res.redirect('/login')
+  } catch (error) {
+    res.sendStatus(500)
+  }
+})
+
+app.post(
+  '/login',
+  passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
-    failureFlash: true
-}))
+    failureFlash: true,
+  })
+)
 
 app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
+  req.logOut()
+  res.redirect('/login')
 })
 
-initialise(passport, 
-    (emailID) => allUsers.find(user => user.emailID === emailID),
-    (userID) => allUsers.find(user => user.userID === userID))
+initialise(
+  passport,
+  (emailID) => allUsers.find((user) => user.emailID === emailID),
+  (userID) => allUsers.find((user) => user.userID === userID)
+)
 
-app.listen(3300)
+app.listen(3300, () => console.log('App is running on port: ', 3300))
